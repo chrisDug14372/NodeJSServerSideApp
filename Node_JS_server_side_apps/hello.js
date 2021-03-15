e1ba68bd87322253c7e64f7cd98710edcc073908
chrisDug14372 <80608840+chrisDug14372@users.noreply.github.com>
2021-03-16 08:39:46 +1000

    Copying across project files to GIT

---
+++ Node_JS_server_side_apps/hello.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var moment = require('moment');
+
+function serverCallback(req, res){
+    var begin_time = moment("10:00", "HH:mm");
+    var end_time = moment("18:00", "HH:mm");
+    
+    var message = "Hello " + process.argv[2] + "!\n";
+    message += "Welcome to our page .\n";
+    message += "Now, it is " + moment().format("HH:mm") + ".\n";
+    message += "Our business hours is from " + begin_time.format("HH:mm") + " to " + end_time.format("HH:mm") + ".\n";
+    
+    var begin_difference = begin_time.diff(moment(), 'minutes');
+    var end_difference = moment().diff(end_time, 'minutes');
+    
+    if(begin_difference>0){
+        message+="Please come back in" + begin_difference + " minutes .\n";
+    }    
+    
+    if(end_difference>0){
+        message+="Please come back tomorrow .\n";
+    }
+    
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.end(message);
+}
+
+http.createServer(serverCallback).listen(8080);
